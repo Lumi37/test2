ad08af58557fff453656e3c533d6a132a803fe35
kostas <eoppep@eoppep.gr>
2022-11-23 13:07:48 +0200

    basic operations

--- js-basics/js/index.js
+++ js-basics/js/index.js
@@ -1,63 +1,64 @@
-//ch_1
+console.log(document.querySelectorAll(".btns button "))
+let temp1 = 0;
+let temp2 = 0;
+let symbolPressed = false ; 
+let allBtns = document.querySelectorAll(".btns button")
+let label = document.querySelector("#display") //document.getElementById("display")
+allBtns.forEach((btn)=> {
+    btn.addEventListener("click", (event)=>{
+        console.log(label.textContent)
+        label.textContent += event.target.textContent
+        
+        if (Number.isInteger( Number.parseInt(event.target.textContent) ) && symbolPressed == false )
+            temp1 = Number(label.textContent)
+        else if(Number.isInteger( Number.parseInt(event.target.textContent) ) && symbolPressed == true)  
+            temp2 = Number(label.textContent)
 
-/*let arr = new Array()
-let limit = prompt("Input your limit",10)
-console.log(limit)
-for(let i = 0; i<=limit; i++){
-  arr[i] = i;
-  if(arr[i]%2 == 1)
-    console.log(arr[i])
-}*/
+        
+        if (event.target.textContent === "+"){
+            symbolPressed = true ;
+            symbol = "+"
+            label.textContent = "0"
+        }
 
-//ch_2
+        if (event.target.textContent === "-"){
+            symbolPressed = true ;
+            symbol = "-"
+            label.textContent = "0"
+        }
 
-/*
-function printPropaideia(arr){
+        if (event.target.textContent === "X"){
+            symbolPressed = true ;
+            symbol = "X"
+            label.textContent = "0"
+        }
 
-  Object.keys(arr).forEach(function(key){
-     console.log(key,"=",arr[key])
-  });
-  console.log(Object.keys(arr)) 
-}
+        if (event.target.textContent === "/"){
+            symbolPressed = true ;
+            symbol = "/"
+            label.textContent = "0"
+        }
+      
+        if (event.target.textContent === "="){
+            if(symbol === "+")
+                label.textContent = temp1 + temp2
+            if(symbol === "-")  
+                label.textContent = temp1 - temp2
+            if(symbol === "X")
+                label.textContent = temp1 * temp2
+            if(symbol === "/"){
+                if(temp2 === 0)
+                    alert("Cant divide with 0 expected result: infinity")
+                label.textContent = temp1 / temp2
+            }
+                
+        }
 
 
 
 
-let propaideia = [];
-for (let k = 0 ; k <10; k++){
 
-  propaideia[k] = {};
-  
-  for (let j=1; j <= 10; j++){
-    propaideia[k] [ `${ k + 1 }x${ j }`]  =(k+1)*j;          // e.g. property of propaideia[1][`1x1'] = 1*1/// propaideia[1]{ "1x1" = 1 etc..}
-  }
-  
-}
-propaideia.forEach(printPropaideia)
-*/
 
-//ch_3
-
-/*
-let kmtomiles,milestokm;
-let pass = 0
-do{
-  choice = prompt("choose: km to miles(k)| miles to km (m)",)
-  if(choice == "k" | choice =="m")
-    pass = 1
-}while(pass == 0);
-if( choice == "k"){
-  kmtomiles = prompt("Enter amount of km to be converted to Miles: ",)
-  alert(kmtomiles*0.62137 + " Miles")
-}
-if(choice == "m"){
-  milestokm = prompt("Enter amount of miles to be converted to Kilometers ",)
-  alert(milestokm*1.60934 + " Kilometers")
-}
-*/
-
-//ch_4
-
-let arr = new Array()
-let limit = prompt("Input your limit",10)
-console.log(limit)
+    })
+    
+});
